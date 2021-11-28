bd10ac678492bb6d2d29abe1beb3c7d06c1d49c6
unknown <hohoaisan@gmail.com>
2021-11-29 00:35:04 +0700

    interceptors and init services

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import {Navigation} from './navigation';
 import AppBar from 'components/Appbar';
 
+import AuthService from 'services/auth.service';
+import {setupAxiosInterceptors} from 'apis/_axios/interceptors';
+
+// init services
+setupAxiosInterceptors();
+AuthService.init();
+
 const App = () => {
   return (
     <>
