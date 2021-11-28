ad9748ada1528094126e78bb242a98bc18b44d9e
unknown <hohoaisan@gmail.com>
2021-11-28 19:17:39 +0700

    Move providers to index

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 /**
  * @format
  */
@@ -5,5 +7,19 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {Provider as ReduxProvider} from 'react-redux';
+import {store} from './src/redux/store';
+import {NativeBaseProvider} from 'native-base';
+import theme from './src/themes/nativebase';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppProvider: React.FC = () => {
+  return (
+    <ReduxProvider store={store}>
+      <NativeBaseProvider theme={theme}>
+        <App />
+      </NativeBaseProvider>
+    </ReduxProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppProvider);
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {NativeBaseProvider} from 'native-base';
-import theme from 'themes/nativebase';
 import {Navigation} from './navigation';
 import AppBar from 'components/Appbar';
 
 const App = () => {
   return (
-    <NativeBaseProvider theme={theme}>
+    <>
       <AppBar />
       <Navigation />
-    </NativeBaseProvider>
+    </>
   );
 };
 
