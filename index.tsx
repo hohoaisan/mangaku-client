f167170eab9118c55a9f01f818076e7402872f23
unknown <hohoaisan@gmail.com>
2021-12-30 23:30:26 +0700

    App ingnore FlatList inside ScrollView

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {Provider as ReduxProvider} from 'react-redux';
@@ -13,6 +13,9 @@ import {NativeBaseProvider} from 'native-base';
 import theme from './src/themes/nativebase';
 
 const AppProvider: React.FC = () => {
+  useEffect(() => {
+    LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+  }, []);
   return (
     <ReduxProvider store={store}>
       <NativeBaseProvider theme={theme}>
